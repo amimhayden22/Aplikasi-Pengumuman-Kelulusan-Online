7e218b7ee4a931021974c1dd022252fce4ba6cf4
amimhayden22 <amim.opreker22@gmail.com>
2021-06-15 16:27:02 +0700

    Add Table for APeK Online

--- database/migrations/2021_05_26_101901_create_students_table.php
+++ database/migrations/2021_05_26_101901_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('nisn');
             $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['Admin', 'Wali Kelas', 'Siswa']);
-            $table->rememberToken();
+            $table->date('date_of_birth');
+            $table->enum('gender', ['Laki-laki', 'Perempuan']);
+            $table->text('attachment');
+            $table->integer('grade_id');
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 }
