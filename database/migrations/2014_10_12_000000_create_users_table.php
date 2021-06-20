606408609e00091f80b52cfad92baae60902a416
amimhayden22 <amim.opreker22@gmail.com>
2021-06-20 14:47:26 +0700

    update value role in table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['Admin', 'Wali Kelas', 'Siswa']);
+            $table->enum('role', ['Admin', 'User']);
             $table->rememberToken();
             $table->timestamps();
         });
